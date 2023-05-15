bb1ffabea96d9b645c3359ae4f0e730551306aba
Infamous0192 <infamous0192@gmail.com>
2023-05-15 11:55:37 +0800

    Integrate profile

--- src/features/employee/pages/Profile.tsx
+++ src/features/employee/pages/Profile.tsx
@@ -1,14 +1,18 @@
 import { UnstyledButton } from '@mantine/core';
 import { IconChevronRight, IconLogout, IconSettings, IconUser } from '@tabler/icons-react';
 
+import { useAuth } from '@/features/auth';
+
 export const Profile: React.FC = () => {
+  const { creds, logout } = useAuth();
+
   return (
     <main className="py-12">
       <section className="flex flex-col items-center justify-center">
         <div className="bg-gray-200 text-gray-900 rounded-full p-7 mb-4">
           <IconUser className="w-16 h-16" />
         </div>
-        <div className="font-bold text-lg">Dwa Meizadewa</div>
+        <div className="font-bold text-lg">{creds?.name}</div>
         <div className="text-sm text-gray-600">Admin Outlet</div>
       </section>
 
@@ -22,7 +26,7 @@ export const Profile: React.FC = () => {
             <IconChevronRight className="w-6 h-6" />
           </div>
         </UnstyledButton>
-        <UnstyledButton className="flex w-full items-center py-2">
+        <UnstyledButton onClick={() => logout()} className="flex w-full items-center py-2">
           <div className="bg-red-50 text-red-600 rounded-lg p-2">
             <IconLogout className="w-6 h-6" />
           </div>
