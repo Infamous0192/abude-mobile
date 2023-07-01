b601bb7d5202ef966fd39598a1b546b38363d7dd
Infamous0192 <infamous0192@gmail.com>
2023-07-01 17:00:32 +0800

    Ignore dist

--- dist/assets/IconChevronLeft-0d65b5dc.js
+++
@@ -1,1 +0,0 @@
-import{f as n}from"./index-37d7c688.js";function r(e,t,a){return Math.min(Math.max(e,t),a)}var c=n("chevron-left","IconChevronLeft",[["path",{d:"M15 6l-6 6l6 6",key:"svg-0"}]]);export{c as I,r as c};
