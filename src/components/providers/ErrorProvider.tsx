d468b733d670df2c31b5207b7838d5ffab831d30
Dwa Meizadewa <infamous0192@gmail.com>
2023-12-28 18:42:34 +0800

    chore: improve error handling

--- src/components/providers/ErrorProvider.tsx
+++ src/components/providers/ErrorProvider.tsx
@@ -1,14 +1,15 @@
 import { Button } from '@mantine/core';
-import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
 
 interface Props {
   children: React.ReactNode;
 }
 
-const ErrorFallback = () => {
+const ErrorFallback: React.FC<FallbackProps> = ({ error }) => {
   return (
     <div className="text-red-500 flex flex-col h-screen justify-center items-center" role="alert">
-      <h2 className="text-lg font-semibold">Terjadi Kesalahan</h2>
+      <h2 className="text-lg font-semibold">{error.name}</h2>
+      <p>{error.message}</p>
       <Button className="mt-4" onClick={() => window.location.assign(window.location.origin)}>
         Refresh
       </Button>
